a2be6eb5b46ab6261db6692007e8c07d0775120a
gap1512 <gap1512@gmail.com>
2022-01-19 07:57:36 -0300

    fix: fix implementation of probability amplitude

--- cl-quantum.lisp
+++ cl-quantum.lisp
@@ -21,9 +21,7 @@
   (atan (imagpart complex-number) (realpart complex-number)))
 
 (defclass/std qubit ()
-  ((theta :std 0.0)
-   (phi :std 0.0)
-   (a :std 1.0)
+  ((a :std 1.0)
    (b :std #C(0.0 0.0))))
 
 (defmethod print-object ((qubit qubit) stream)
@@ -34,12 +32,19 @@
   (with-slots (a b) qubit
     (list a b)))
 
-(defmethod phi ((qubit qubit))
-  (acos (a qubit)))
-
 (defmethod theta ((qubit qubit))
+  (* 2 (acos (a qubit))))
+
+(defmethod phi ((qubit qubit))
   (with-slots (b) qubit
-    (/ (log (/ b (custom-sin (phi qubit)))) i)))
+    (atan (imagpart b) (realpart b))))
+
+(defmethod cartesian ((qubit qubit))
+  (let ((theta (theta qubit))
+	(phi (phi qubit)))
+    (values (* (custom-sin theta) (custom-cos phi))
+	    (* (custom-sin theta) (custom-sin phi))
+	    (custom-cos theta))))
 
 (defmethod set-bloch-sphere ((qubit qubit) theta phi)
   (let ((normalized-theta (if (<= .0 theta pi)
@@ -48,22 +53,21 @@
 	(normalized-phi (if (<= (- pi) phi pi)
 			phi
 			(- (mod phi (* 2 pi)) (* 2 pi)))))
-    (setf (a qubit) (custom-cos normalized-phi)
-	  (b qubit) (* (custom-sin normalized-phi) (exp (* normalized-theta i))))))
+    (setf (a qubit) (custom-cos (/ normalized-theta 2))
+	  (b qubit) (* (custom-sin (/ normalized-theta 2))
+		       (exp (* i normalized-phi))))))
 
 (defun quadratic-norm (number)
   (expt (abs number) 2))
 
-(defmethod set-a-b ((qubit qubit) a b)
+(defmethod set-probability-amplitude ((qubit qubit) a b)
   (let* ((quadratic-norm-a (quadratic-norm a))
 	 (quadratic-norm-b (quadratic-norm b))
-	 (calculated-probability (+ quadratic-norm-a quadratic-norm-b))
-	 (normalized-a (sqrt (/ quadratic-norm-a calculated-probability)))
-	 (normalized-b (sqrt (/ quadratic-norm-b calculated-probability))))
-    (setf (a qubit) (abs normalized-a)
-	  (b qubit) (* (abs normalized-b)
-		       (exp (* i (- (custom-angle normalized-b)
-				    (custom-angle normalized-a))))))))
+	 (probability (+ quadratic-norm-a quadratic-norm-b)))
+    (setf (a qubit) (sqrt (/ quadratic-norm-a probability))
+	  (b qubit) (* (sqrt (/ quadratic-norm-b probability))
+		       (/ b (abs b))
+		       (/ (abs a) a)))))
 
 (defmethod measure ((qubit qubit))
   (with-slots (a b) qubit
@@ -72,8 +76,3 @@
       (if (<= probability probability-of-a)
 	  '0
 	  '1))))
-
-(defun test ()
-  (let ((qubit (make-instance 'qubit)))
-    (set-a-b qubit (/ (sqrt 2) 2) (/ (sqrt 2) 2))
-    (measure qubit)))
